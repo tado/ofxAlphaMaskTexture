d047a9d8beb945f92f4ee2e8e72f9271bc16aefa
Atsushi Tadokoro <tadokoro@gmail.com>
2015-12-03 20:48:23 +0900

    update comment

--- example-maskScale/src/ofApp.cpp
+++ example-maskScale/src/ofApp.cpp
@@ -9,12 +9,12 @@ void ofApp::setup(){
     bottomImg.load("space.jpg");
     maskImg.load("mask.jpg");
     
-    alphaMask = new ofxAlphaMaskTexture(cam.getTexture(),        // top layer texture
-                                        bottomImg.getTexture(),  // bottom layer texture
-                                        maskImg.getTexture());   // mask layer texture
+    alphaMask = new ofxAlphaMaskTexture(cam.getTexture(),       // top layer texture
+                                        bottomImg.getTexture(), // bottom layer texture
+                                        maskImg.getTexture());  // mask layer texture
     
-    gui.setup(); // most of the time you don't need a name
-    gui.add(maskScale.setup("Mask Scale", 1.0, 0.0, 3.0));
+    gui.setup();
+    gui.add(maskScale.setup("Mask Scale", 1.0, 0.0, 3.0));      // set mask scale
 }
 
 //--------------------------------------------------------------
@@ -33,55 +33,55 @@ void ofApp::draw(){
 
 //--------------------------------------------------------------
 void ofApp::keyPressed(int key){
-
+    
 }
 
 //--------------------------------------------------------------
 void ofApp::keyReleased(int key){
-
+    
 }
 
 //--------------------------------------------------------------
 void ofApp::mouseMoved(int x, int y ){
-
+    
 }
 
 //--------------------------------------------------------------
 void ofApp::mouseDragged(int x, int y, int button){
-
+    
 }
 
 //--------------------------------------------------------------
 void ofApp::mousePressed(int x, int y, int button){
-
+    
 }
 
 //--------------------------------------------------------------
 void ofApp::mouseReleased(int x, int y, int button){
-
+    
 }
 
 //--------------------------------------------------------------
 void ofApp::mouseEntered(int x, int y){
-
+    
 }
 
 //--------------------------------------------------------------
 void ofApp::mouseExited(int x, int y){
-
+    
 }
 
 //--------------------------------------------------------------
 void ofApp::windowResized(int w, int h){
-
+    
 }
 
 //--------------------------------------------------------------
 void ofApp::gotMessage(ofMessage msg){
-
+    
 }
 
 //--------------------------------------------------------------
 void ofApp::dragEvent(ofDragInfo dragInfo){ 
-
+    
 }
